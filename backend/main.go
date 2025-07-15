9fa80ff9ee2c98c4a9de8ffdac39d00a3e3d097a
Xenonyy <gonda.armand@gmail.com>
2025-07-15 15:17:00 +0200

    fix: cors error

--- backend/main.go
+++ backend/main.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"database/sql"
+	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 var db *sql.DB
 
 func main() {
+	godotenv.Load()
 	gin.SetMode(gin.ReleaseMode)
 	db = ConnectDB()
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{os.Getenv("ALLOWED_ORIGINS")},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
 	RegisterRoutes(router)
 	router.Run(":8080")
 }

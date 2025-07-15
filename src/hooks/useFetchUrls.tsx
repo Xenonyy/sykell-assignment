505282c87f86bceecadeddd399f6e6110ffef14a
Xenonyy <gonda.armand@gmail.com>
2025-07-15 23:45:54 +0200

    fix: use translations

--- src/hooks/useFetchUrls.tsx
+++ src/hooks/useFetchUrls.tsx
@@ -1,11 +1,13 @@
 import { useQuery, type UseQueryResult } from '@tanstack/react-query';
+import { messages } from '../messages';
 
 export const useFetchUrls = (): UseQueryResult<any, Error> => {
   const data = useQuery({
     queryKey: ['urls'],
+    refetchInterval: 500,
     queryFn: async () => {
       const res = await fetch('http://localhost:8080/urls');
-      if (!res.ok) throw new Error('Failed to fetch URLs');
+      if (!res.ok) throw new Error(messages.fetchError);
       return res.json();
     },
     refetchOnWindowFocus: false,
